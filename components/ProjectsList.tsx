b3677fae0739973a5002662ca64066b05b4ba45a
Eyal Label <sslabel2@gmail.com>
2025-07-28 17:31:31 +0300

    minor fix to project list

--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 
 import { ProjectPreview } from "@/components/project-preview";
-import { ProjectPreviewList } from "@/config/site";
+import { ProjectPreviewList, ProjectType } from "@/config/site";
 
 const MotionDiv = motion.div;
 
@@ -14,21 +14,23 @@ export function ProjectList({ type = "game" }: { type?: "game" | "web" }) {
       transition={{ duration: 0.6, delay: 0.2 }}
     >
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 justify-items-center">
-        {ProjectPreviewList.previews.filter((preview) => preview.type === type).map((preview) => (
-          <motion.div
-            key={preview.index}
-            animate={{ opacity: 1, y: 0 }}
-            className="w-full max-w-sm"
-            initial={{ opacity: 0, y: 30 }}
-            transition={{
-              duration: 0.5,
-              delay: 0.1 * preview.index,
-              ease: "easeOut",
-            }}
-          >
-            <ProjectPreview {...preview} />
-          </motion.div>
-        ))}
+        {ProjectPreviewList.previews
+          .filter((preview) => preview.type === type)
+          .map((preview) => (
+            <motion.div
+              key={preview.index}
+              animate={{ opacity: 1, y: 0 }}
+              className="w-full max-w-sm"
+              initial={{ opacity: 0, y: 30 }}
+              transition={{
+                duration: 0.5,
+                delay: 0.1 * preview.index,
+                ease: "easeOut",
+              }}
+            >
+              <ProjectPreview {...{ ...preview, type: preview.type as ProjectType }} />
+            </motion.div>
+          ))}
       </div>
     </MotionDiv>
   );
