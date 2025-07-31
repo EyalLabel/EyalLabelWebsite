b07576741d14af80e0f7cdfe98be97ceac4257bb
Eyal Label <sslabel2@gmail.com>
2025-07-31 13:00:41 +0300

    dynamic

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: "export",
     images: { unoptimized: true },
     trailingSlash: true,
     // basePath removed for GitHub Pages deployment to root domain
